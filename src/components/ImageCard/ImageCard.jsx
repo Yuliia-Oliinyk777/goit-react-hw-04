01e20a7fe069f83f7d9a06166a499dafb2cc5521
Yuliia-Oliinyk777 <yuliya2474@gmail.com>
2024-10-29 11:04:09 +0100

    update imageCard.jsx

--- src/components/ImageCard/ImageCard.jsx
+++ src/components/ImageCard/ImageCard.jsx
@@ -1,8 +1,9 @@
 import style from './ImageCard.module.css';
 const ImageCard = ({ photo, onClick }) => {
   return (
-    <div onClick={() => onClick(photo)}>
+    <div>
       <img
+        onClick={() => onClick(photo)}
         className={style.image}
         src={photo.urls.small}
         alt={photo.alt_description}
